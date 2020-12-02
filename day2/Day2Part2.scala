7617534c0a91a1c043709d4c5a16e180e5f092dc
Rui Alves <up201606746@fe.up.pt>
2020-12-02 12:13:00 +0000

    Add day 2 part 2 solution

---
+++ day2/Day2Part2.scala
@@ -0,0 +1,25 @@
+package day2
+
+import utils.FileReader
+
+object Day2Part2 {
+    final val PASS_RULE_REGEX = "(\\d+)-(\\d+) (\\w): (\\w+)".r
+
+    def main(args: Array[String]): Unit = {
+        val list = FileReader.readFile("Advent-Of-Code-2020/day2/input.txt")
+        val num_valid = list.count(s => isValid(s))
+        println(num_valid)
+    }
+
+    def isValid(pass_rule: String): Boolean = {
+        val results = PASS_RULE_REGEX.findAllMatchIn(pass_rule).toList(0)
+        val idx1 = results.group(1).toInt
+        val idx2 = results.group(2).toInt
+        val char = results.group(3)(0)
+        val str = results.group(4)
+
+        val occ1 = if (str(idx1 - 1).equals(char)) 1 else 0
+        val occ2 = if (str(idx2 - 1).equals(char)) 1 else 0
+        occ1 + occ2 == 1
+    }
+}
