1601b4660c2e67d462a4b081e8633c44d6d822d4
Rui Alves <up201606746@fe.up.pt>
2020-12-21 11:57:57 +0000

    Add day 21 part 2 solution

---
+++ day21/Day21Part2.scala
@@ -0,0 +1,54 @@
+package day21
+
+import utils.FileReader
+
+import scala.collection.mutable
+
+object Day21Part2 {
+    type Food = (mutable.HashSet[String], mutable.HashSet[String])
+
+    final val FOOD_REGEX = "^(.+)\\s\\(contains\\s(.+)\\)$".r
+
+    def main(args: Array[String]): Unit = {
+        val lines = FileReader.readFile("Advent-Of-Code-2020/day21/input.txt").toArray
+        val foods: Array[Food] = lines.map(line => FOOD_REGEX.findFirstMatchIn(line) match {
+            case Some(m) => (
+                m.group(1).split(" ").to(mutable.HashSet),
+                m.group(2).split(", ").to(mutable.HashSet)
+            )
+            case None => throw new Exception(s"Invalid input food: $line")
+        })
+
+        val possible_allergens = new mutable.HashMap[String, mutable.HashSet[String]]()
+        val allergens = new mutable.HashMap[String, String]()
+
+        foods.foreach { case (food_ingredients, food_allergens) => {
+            food_allergens.foreach(allergen => possible_allergens.get(allergen) match {
+                case Some(ing_list) => possible_allergens(allergen) = ing_list.intersect(food_ingredients)
+                case None => possible_allergens(allergen) = food_ingredients.clone
+            })
+        }}
+
+        while (possible_allergens.nonEmpty) {
+            val curr_allergen = possible_allergens.toArray.sortWith(_._2.size < _._2.size).head
+
+            if (curr_allergen._2.size != 1) {
+                throw new Exception("Matching not possible")
+            }
+
+            val ing = curr_allergen._2.last
+            allergens(curr_allergen._1) = ing
+
+            possible_allergens.foreach(_._2.remove(ing))
+            possible_allergens.remove(curr_allergen._1)
+        }
+
+        val canonical_dangerous_ingredient_list = allergens
+            .toArray
+            .sortWith(_._1 < _._1)
+            .map(_._2)
+            .mkString(",")
+
+        println(canonical_dangerous_ingredient_list)
+    }
+}
