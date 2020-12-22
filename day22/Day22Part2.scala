c79bbe443a4e3f074c2d20f96e00c6989bb32d00
Rui Alves <up201606746@fe.up.pt>
2020-12-22 11:09:57 +0000

    Add day 22 part 2 solution

---
+++ day22/Day22Part2.scala
@@ -0,0 +1,68 @@
+package day22
+
+import utils.FileReader
+
+import scala.collection.mutable
+
+object Day22Part2 {
+    type Round = (String, String)
+
+    def main(args: Array[String]): Unit = {
+        val lines = FileReader.readFile("Advent-Of-Code-2020/day22/input.txt").toArray
+        val raw_hands = lines.mkString("\n").split("\n\n")
+
+        val h1 = parseHand(raw_hands(0))
+        val h2 = parseHand(raw_hands(1))
+        playGame(h1, h2)
+
+        val score = if (h1.nonEmpty) calcScore(h1) else calcScore(h2)
+        println(score)
+    }
+
+    def playGame(h1: mutable.Queue[Int], h2: mutable.Queue[Int]): Boolean = {
+        val round_history = new mutable.HashSet[Round]()
+
+        while (h1.nonEmpty && h2.nonEmpty) {
+            val round = (h1.mkString(""), h2.mkString(""))
+            var player1_wins = true
+
+            if (round_history.contains(round)) {
+                return player1_wins
+            }
+            round_history.add(round)
+
+            val c1 = h1.dequeue()
+            val c2 = h2.dequeue()
+
+            if (h1.length >= c1 && h2.length >= c2) {
+                player1_wins = playGame(
+                    h1.slice(0, c1).clone(),
+                    h2.slice(0, c2).clone()
+                )
+            } else {
+                player1_wins = c1 > c2
+            }
+
+            if (player1_wins) {
+                h1.enqueue(c1)
+                h1.enqueue(c2)
+            } else {
+                h2.enqueue(c2)
+                h2.enqueue(c1)
+            }
+        }
+        h1.nonEmpty
+    }
+
+    def calcScore(hand: mutable.Queue[Int]): Int = hand
+        .reverse
+        .zipWithIndex
+        .map{ case(card, idx) => card * (idx + 1) }
+        .sum
+
+    def parseHand(raw_hand: String): mutable.Queue[Int] = raw_hand
+        .split("\n")
+        .drop(1)
+        .map(_.toInt)
+        .to(mutable.Queue)
+}
