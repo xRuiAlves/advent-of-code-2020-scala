97c938cac2b24aeb549677ce6d24a31e660c76d5
Rui Alves <up201606746@fe.up.pt>
2020-12-22 11:09:51 +0000

    Add day 22 part 1 solution

---
+++ day22/Day22Part1.scala
@@ -0,0 +1,49 @@
+package day22
+
+import utils.FileReader
+
+import scala.collection.mutable
+
+object Day22Part1 {
+    def main(args: Array[String]): Unit = {
+        val lines = FileReader.readFile("Advent-Of-Code-2020/day22/input.txt").toArray
+        val raw_hands = lines.mkString("\n").split("\n\n")
+
+        val h1 = parseHand(raw_hands(0))
+        val h2 = parseHand(raw_hands(1))
+        playGame(h1, h2)
+
+        val score = if (h1.nonEmpty) calcScore(h1) else calcScore(h2)
+        println(score)
+    }
+
+    def playGame(h1: mutable.Queue[Int], h2: mutable.Queue[Int]): Boolean = {
+        while (h1.nonEmpty && h2.nonEmpty) {
+            val c1 = h1.dequeue()
+            val c2 = h2.dequeue()
+
+            if (c1 > c2) {
+                h1.enqueue(c1)
+                h1.enqueue(c2)
+            } else if (c2 > c1) {
+                h2.enqueue(c2)
+                h2.enqueue(c1)
+            } else {
+                throw new Exception("Repeated card")
+            }
+        }
+        h1.nonEmpty
+    }
+
+    def calcScore(hand: mutable.Queue[Int]): Int = hand
+        .reverse
+        .zipWithIndex
+        .map{ case(card, idx) => card * (idx + 1) }
+        .sum
+
+    def parseHand(raw_hand: String): mutable.Queue[Int] = raw_hand
+        .split("\n")
+        .drop(1)
+        .map(_.toInt)
+        .to(mutable.Queue)
+}
