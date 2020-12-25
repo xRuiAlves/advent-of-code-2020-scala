0b2407c78ed13d58958f63c428ad70419af8c536
Rui Alves <up201606746@fe.up.pt>
2020-12-25 11:13:37 +0000

    Add day 25 solution

---
+++ day25/Day25.scala
@@ -0,0 +1,43 @@
+package day25
+
+import utils.FileReader
+
+object Day25 {
+    final val LOOP_DIVISOR = 20201227L
+    final val DEFAULT_SUBJECT = 7L
+    final val INIT_VALUE = 1L
+
+    def main(args: Array[String]): Unit = {
+        val lines = FileReader.readFile("Advent-Of-Code-2020/day25/input.txt").toArray
+        val pub_key1 = lines.head.toLong
+        val pub_key2 = lines.last.toLong
+
+        val l1 = findLoopSize(pub_key1)
+        val l2 = findLoopSize(pub_key2)
+
+        val v1 = loop(pub_key1, l2)
+        val v2 = loop(pub_key2, l1)
+
+        assert(v1 == v2)
+        println(v1)
+    }
+
+    def transform(value: Long, subject: Long): Long = (value * subject) % LOOP_DIVISOR
+
+    def findLoopSize(pub_key: Long): Int = {
+        var value = INIT_VALUE
+        var loop = 0
+
+        while (value != pub_key) {
+            value = transform(value, DEFAULT_SUBJECT)
+            loop += 1
+        }
+        loop
+    }
+
+    def loop(subject: Long, num_loops: Int): Long = {
+        var value = INIT_VALUE
+        for (_ <- 0 until num_loops) value = transform(value, subject)
+        value
+    }
+}
